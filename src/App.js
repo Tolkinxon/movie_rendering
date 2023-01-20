c143e41dbb0a4d3ded5c913fb7aaf76e711218e5
Tolkinxon <krosstony639@gmail.com>
2023-01-20 19:59:55 +0500

    added new use Reducer

--- src/App.js
+++ src/App.js
@@ -1,33 +1,28 @@
 import React, { useReducer } from 'react'
-import reducer from './Reducer'
-
 import './App.css'
+import reducer20 from './reducer2.0'
 
-function App() {
-  const [{ r, g, b }, dispatch] = useReducer(reducer, { r: 0, g: 0, b: 0 })
-
+export default function App() {
+  const [{ top, bottom, left, right }, dispatch] = useReducer(reducer20, {
+    top: 0,
+    bottom: 2,
+    left: 2,
+    right: 2,
+  })
   return (
-    <div className="App">
+    <div className="app">
       <div className="container">
-        <h1 style={{ color: `rgb(${r}, ${g}, ${b})` }}>hello world</h1>
-        <div className="colors">
-          <p>R</p>
-          <button onClick={() => dispatch({ type: 'INCREMENT_R' })}>+</button>
-          <button onClick={() => dispatch({ type: 'DECREMENT_R' })}>-</button>
-        </div>
-        <div className="colors">
-          <p>G</p>
-          <button onClick={() => dispatch({ type: 'INCREMENT_G' })}>+</button>
-          <button onClick={() => dispatch({ type: 'DECREMENT_G' })}>-</button>
-        </div>
-        <div className="colors">
-          <p>B</p>
-          <button onClick={() => dispatch({ type: 'INCREMENT_B' })}>+</button>
-          <button onClick={() => dispatch({ type: 'DECREMENT_B' })}>-</button>
-        </div>
+        <div
+          className="shape"
+          style={{
+            borderRadius: `${top}px ${bottom}px ${left}px ${right}px`,
+          }}
+        ></div>
+        <button onClick={() => dispatch({ type: 'top' })}>top</button>
+        <button onClick={() => dispatch({ type: 'bottom' })}>bottom</button>
+        <button onClick={() => dispatch({ type: 'left' })}>left</button>
+        <button onClick={() => dispatch({ type: 'right' })}>right</button>
       </div>
     </div>
   )
 }
-
-export default App

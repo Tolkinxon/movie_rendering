f0834383044aef207b0fd79b66ed3117fb330072
Tolkinxon <krosstony639@gmail.com>
2024-04-29 15:31:46 +0300

    img element added

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 const list = document.querySelector('#movie__list')
-const movieArr = movies.slice(0, 10)
+const movieArr = movies.slice(0, 20)
 
 
 function render(arr, renderingPlace) {
     
     arr.forEach(item => {
-        const {Title, movie_year, language} = item
+        const {Title, movie_year, language, ytid} = item
         
         const newLi = document.createElement('li')
+        const newImg = document.createElement('img')
         const newH1 = document.createElement('h1')
         const movieYear = document.createElement('p')
         const languageTitle = document.createElement('p')
         
-        
+        newImg.src = `https://img.youtube.com/vi/${ytid}/maxresdefault.jpg`
+        newImg.width = 500
         newH1.textContent = Title
         movieYear.textContent = movie_year
         languageTitle.textContent = language
         
+        newLi.appendChild(newImg)
         newLi.appendChild(newH1)
         newLi.appendChild(movieYear)
         newLi.appendChild(languageTitle)

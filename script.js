a7c48caaf06ea1b100c15a4fe92ec16ab089d750
Tolkinxon <krosstony639@gmail.com>
2024-05-04 07:40:17 +0300

    taking category list from movie list

--- script.js
+++ script.js
@@ -1,38 +1,91 @@
+const elFilterButton = document.querySelector('.movie__filter-button')
+const elRating = document.querySelector('.movie__rating-input')
 const elList = document.querySelector('#movie__list')
 const elCounter = document.querySelector('#counter')
 const elTemplate = document.querySelector('#template').content
 
 
-let movieArr = movies.slice(0, 10)
+let movieArr = movies.slice(0, 10) // taking of part movie list
 
-counter.textContent = movieArr.length
+elCounter.textContent = movieArr.length // counting length of movies
 
-const nomalizedMovieList = movieArr.map(item => {
-    const {Title, movie_year,  ytid, imdb_rating} = item
+
+
+
+// normalized list for using better ------------------------------------------------------
+const normalizedMovieList = movieArr.map(item => {
+    const {Title, movie_year,  ytid, imdb_rating, Categories} = item
     return {
-        title: Title,
+        title: Title.toString(),
         year: movie_year,
         ytId: ytid,
-        rating: imdb_rating
+        rating: imdb_rating,
+        categories: Categories
     }
 })
+//----------------------------------------------------------------------------------------
 
 
 
+
+//rendering place ---------------------------------------------------------------------------
 function render(arr, renderingPlace){
+
+    renderingPlace.innerHTML = null
+    
+    const fragment = document.createDocumentFragment()
+
     arr.forEach(item => {
-        
+
         const {title, year,  ytId, rating} = item
 
         const newTemplate = elTemplate.cloneNode(true)
 
-        console.log(newTemplate);
+        newTemplate.querySelector('.movie__img').src = `https://img.youtube.com/vi/${ytId}/mqdefault.jpg`
+        newTemplate.querySelector('.movie__title').textContent = title
+        newTemplate.querySelector('.movie__year').textContent = year
+        newTemplate.querySelector('.movie__rating').textContent = rating
 
-        
-    
+        fragment.appendChild(newTemplate)
     })
-    
+    renderingPlace.appendChild(fragment)
 }
 
-render(nomalizedMovieList, elList)
+render(normalizedMovieList, elList) // rendering
+//----------------------------------------------------------------------------------------
+
+
+
+
+
+// filter for rating -----------------------------------------------------------------------
+elFilterButton.addEventListener('click', (evt) => {
+
+    const ratingValue = +elRating.value.trim()
+
+   const filteredMovie = normalizedMovieList.filter(item => item.rating >= ratingValue) 
+
+    render(filteredMovie, elList) // rendering
+    elCounter.textContent = filteredMovie.length // counting
+})
+//--------------------------------------------------------------------------------------------
+
+
+
+
+// taking category list from movie list ------------------------------------------------------
+    const categoryList = []
+
+   normalizedMovieList.forEach(item => {
+      let oneItemCategory = item.categories.split('|')
+    
+      oneItemCategory.forEach(item => {
+        console.log(categoryList.includes(item));
+
+      })
+    })
+
+   
+// -------------------------------------------------------------------------------------------
+
 

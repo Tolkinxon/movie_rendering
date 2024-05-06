c90f89056fa0ee2d73130059ce21758b54128aec
Tolkinxon <krosstony639@gmail.com>
2024-05-06 14:44:18 +0300

    adding filter by name function

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+const elMovieName = document.querySelector('#movie__name')
+const elRating = document.querySelector('#movie__rating-input')
+const elCategoriesSelect = document.querySelector('#categories-select')
 const elFilterButton = document.querySelector('.movie__filter-button')
-const elRating = document.querySelector('.movie__rating-input')
-const elList = document.querySelector('#movie__list')
 const elCounter = document.querySelector('#counter')
+const elList = document.querySelector('#movie__list')
 const elTemplate = document.querySelector('#template').content
 
 
-let movieArr = movies.slice(0, 3) // taking of part movie list
+let movieArr = movies.slice(0, 50) // taking of part movie list
 
 elCounter.textContent = movieArr.length // counting length of movies
 
@@ -16,11 +18,11 @@ elCounter.textContent = movieArr.length // counting length of movies
 const normalizedMovieList = movieArr.map(item => {
     const {Title, movie_year,  ytid, imdb_rating, Categories} = item
     return {
-        title: Title.toString(),
-        year: movie_year,
-        ytId: ytid,
-        rating: imdb_rating,
-        categories: Categories
+        movie__title: Title.toString(),
+        movie__year: movie_year,
+        movie__ytId: ytid,
+        movie__rating: imdb_rating,
+        movie__categories: Categories
     }
 })
 //----------------------------------------------------------------------------------------
@@ -28,7 +30,7 @@ const normalizedMovieList = movieArr.map(item => {
 
 
 
-//rendering place ---------------------------------------------------------------------------
+//rendering place for movie list  ---------------------------------------------------------------------------
 function render(arr, renderingPlace){
 
     renderingPlace.innerHTML = null
@@ -37,37 +39,40 @@ function render(arr, renderingPlace){
 
     arr.forEach(item => {
 
-        const {title, year,  ytId, rating} = item
+        const {movie__title,  movie__ytId, movie__rating, movie__categories} = item
 
         const newTemplate = elTemplate.cloneNode(true)
 
-        newTemplate.querySelector('.movie__img').src = `https://img.youtube.com/vi/${ytId}/mqdefault.jpg`
-        newTemplate.querySelector('.movie__title').textContent = title
-        newTemplate.querySelector('.movie__year').textContent = year
-        newTemplate.querySelector('.movie__rating').textContent = rating
+        newTemplate.querySelector('.movie__img').src = `https://img.youtube.com/vi/${movie__ytId}/mqdefault.jpg`
+        newTemplate.querySelector('.movie__title').textContent = movie__title
+        newTemplate.querySelector('.movie__category').textContent = movie__categories.split('|').join(', ')
+        newTemplate.querySelector('.movie__rating').textContent = movie__rating
 
         fragment.appendChild(newTemplate)
     })
     renderingPlace.appendChild(fragment)
 }
 
-render(normalizedMovieList, elList) // rendering
+
+
+
+render(normalizedMovieList, elList) // rendering movie list
 //----------------------------------------------------------------------------------------
 
 
 
 
 
-// filter for rating -----------------------------------------------------------------------
-elFilterButton.addEventListener('click', (evt) => {
+// filter by rating -----------------------------------------------------------------------
+// elFilterButton.addEventListener('click', (evt) => {
 
-    const ratingValue = +elRating.value.trim()
+//     const ratingValue = +elRating.value.trim()
 
-   const filteredMovie = normalizedMovieList.filter(item => item.rating >= ratingValue) 
+//    const filteredMovie = normalizedMovieList.filter(item => item.rating >= ratingValue) 
 
-    render(filteredMovie, elList) // rendering
-    elCounter.textContent = filteredMovie.length // counting
-})
+//     render(filteredMovie, elList) // rendering
+//     elCounter.textContent = filteredMovie.length // counting
+// })
 //--------------------------------------------------------------------------------------------
 
 
@@ -77,7 +82,7 @@ elFilterButton.addEventListener('click', (evt) => {
     const categoryList = []
 
    normalizedMovieList.forEach(item => {
-      let oneItemCategory = item.categories.split('|')
+      let oneItemCategory = item.movie__categories.split('|')
     
       oneItemCategory.forEach(item => {
         if(!(categoryList.includes(item))){
@@ -85,8 +90,64 @@ elFilterButton.addEventListener('click', (evt) => {
         }
       })
     })
-    
-   
 // -------------------------------------------------------------------------------------------
 
 
+
+
+// rendering category list to select for choosing category -----------------------------------
+function renderCategoriesList(categoriesList, renderingPlace) {
+    
+    const fragment = document.createDocumentFragment()
+
+    categoriesList.forEach(item => {
+        const newOption = document.createElement('option')
+
+        newOption.value = item
+        newOption.textContent = item
+
+        fragment.appendChild(newOption)
+    })
+
+    renderingPlace.appendChild(fragment)
+}
+
+renderCategoriesList(categoryList, elCategoriesSelect) // rendering category list to select tag
+//-------------------------------------------------------------------------------------------------
+
+
+
+
+// filtering movie list by selectd category -------------------------------------------------
+// elFilterButton.addEventListener('click', (evt) => {
+
+//     let filteredListByCategory = []
+
+//     if(elCategoriesSelect.value == 'All') {
+//         filteredListByCategory = normalizedMovieList
+//     }
+//     else {
+//         filteredListByCategory = normalizedMovieList.filter(item => {
+//           return item.categories.split('|').includes(elCategoriesSelect.value)
+//         })
+//     }
+
+//     render(filteredListByCategory, elList) // rendering movie list by selectd category
+// })
+//-----------------------------------------------------------------------------------------------
+
+
+elMovieName.addEventListener('change', (evt) => {
+
+
+    
+
+    const pattern = new RegExp(elMovieName.value.trim(), 'gi')
+
+
+    const filteredNorm = normalizedMovieList.filter(item => {
+        return item.movie__title.match(pattern)
+    })
+    
+    render(filteredNorm, elList) 
+})
